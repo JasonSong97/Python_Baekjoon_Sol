e9b10af44c970d64aabf6184237ab2fd9bbe00fe
JasonSong97 <jaegeunsong97@gmail.com>
2022-10-06 21:13:57 +0900

    baekjoon 11047
    baekjoon 11047, 그리디알고리즘
    v

---
+++ baekjoon11047/baekjoon11047.py
@@ -0,0 +1,25 @@
+# 1
+import sys
+n,k=map(int,input().split())
+coin=[]
+for i in range(n):
+    coin.append(int(sys.stdin.readline().rstrip()))
+cnt=0
+for i in range(n-1,-1,-1):
+    cnt+=k//coin[i] #개수추가
+    k=k-k//coin[i]*coin[i] #그만큼 빼주기
+  
+print(cnt)
+
+# 2
+n, k = map(int, input().split())
+a = []
+result = 0
+
+for i in range(n):
+    a.append(int(input()))
+
+for i in reversed(range(n)):
+    result += (k // a[i])
+    k = k % a[i]
+print(result)
