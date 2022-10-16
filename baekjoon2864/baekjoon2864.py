9ceb87c57f1e53d49de23f02e3aef6728c68e1d3
JasonSong97 <jaegeunsong97@gmail.com>
2022-10-16 11:40:36 +0900

    baekjoon 2864
    baekjoon 2864, 그리디알고리즘

---
+++ baekjoon2864/baekjoon2864.py
@@ -0,0 +1,10 @@
+# 1
+a, b = map(str, input().split())
+
+max_a = a.replace('5', '6')
+max_b = b.replace('5', '6')
+
+min_a = a.replace('6', '5')
+min_b = b.replace('6', '5')
+
+print(int(min_a)+int(min_b), int(max_a)+int(max_b), sep = ' ')
